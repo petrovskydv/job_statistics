c6e81af49ffc97eca19855cff6dcdd5ff8ef16b7
Денис Петровский <petrovsky.dv@gmail.com>
2021-01-30 16:14:11 +0300

    добавил расчет расчет средней ЗП по ЯП

--- main.py
+++ main.py
@@ -3,10 +3,12 @@ from pprint import pprint
 
 
 def main():
-    # programming_languages = ['python', 'java', 'javascript', 'C#', 'C++', 'PHP', 'Typescript', 'Ruby']
-    programming_languages = ['python']
+    programming_languages = ['python', 'java', 'javascript', 'C#', 'C++', 'PHP', 'Typescript', 'Ruby']
+    # programming_languages = ['python']
+    vacancy_statistics = {}
     for programming_language in programming_languages:
-        fetch_vacancies(programming_language)
+        vacancy_statistics[programming_language] = fetch_vacancies(programming_language)
+    pprint(vacancy_statistics)
 
 
 def fetch_vacancies(programming_language):
@@ -19,8 +21,18 @@ def fetch_vacancies(programming_language):
     response = requests.get('https://api.hh.ru/vacancies', params=params)
     response.raise_for_status()
     review_result = response.json()
+    average_salaries = {}
+    average_salaries['vacancies_found'] = review_result['found']
+    salaries = []
     for vacancy in review_result['items']:
-        pprint(predict_rub_salary(vacancy))
+        expected_salary = predict_rub_salary(vacancy)
+        if expected_salary:
+            salaries.append(expected_salary)
+    average_salaries['vacancies_processed'] = len(salaries)
+    average_salaries['average_salary'] = int(sum(salaries) / len(salaries))
+    pprint(average_salaries)
+    return average_salaries
+
 
 def predict_rub_salary(vacancy):
     if not vacancy['salary']:
@@ -31,11 +43,11 @@ def predict_rub_salary(vacancy):
     if vacancy['salary']['currency'] != 'RUR':
         return None
     if from_salary and to_salary:
-        return (from_salary+to_salary)/2
+        return (from_salary + to_salary) / 2
     elif from_salary and not to_salary:
-        return from_salary*1.2
+        return from_salary * 1.2
     elif not from_salary and to_salary:
-        return to_salary*0.8
+        return to_salary * 0.8
 
 
 if __name__ == '__main__':
