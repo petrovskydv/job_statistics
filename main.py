091cd5a2dd4318a073015d331de0ce4136d32d51
Денис Петровский <petrovsky.dv@gmail.com>
2021-01-30 15:27:38 +0300

    вывел зп по вакансиям

--- main.py
+++ main.py
@@ -2,21 +2,25 @@ import requests
 from pprint import pprint
 
 
-
-
 def main():
-    programming_languages = ['python', 'java', 'javascript', 'C#', 'C++', 'PHP', 'Typescript', 'Ruby']
+    # programming_languages = ['python', 'java', 'javascript', 'C#', 'C++', 'PHP', 'Typescript', 'Ruby']
+    programming_languages = ['python']
+    for programming_language in programming_languages:
+        fetch_vacancies(programming_language)
+
+
+def fetch_vacancies(programming_language):
     params = {
         'area': 1,
         'specialization': '1.221',
         'period': 30,
-        'text': ' OR '.join(programming_languages)
+        'text': programming_language
     }
     response = requests.get('https://api.hh.ru/vacancies', params=params)
     response.raise_for_status()
     review_result = response.json()
-    pprint(review_result)
-
+    for vacancy in review_result['items']:
+        pprint(vacancy['salary'])
 
 
 if __name__ == '__main__':
