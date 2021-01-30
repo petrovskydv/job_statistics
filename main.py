1d9b4771049d7585efa140eebac1d318aaa58e31
Денис Петровский <petrovsky.dv@gmail.com>
2021-01-30 15:03:15 +0300

    добавил отбор по популярным ЯП

--- main.py
+++ main.py
@@ -1,18 +1,22 @@
 import requests
 from pprint import pprint
-import json
+
+
 
 
 def main():
+    programming_languages = ['python', 'java', 'javascript', 'C#', 'C++', 'PHP', 'Typescript', 'Ruby']
     params = {
         'area': 1,
         'specialization': '1.221',
-        'period': 30
+        'period': 30,
+        'text': ' OR '.join(programming_languages)
     }
     response = requests.get('https://api.hh.ru/vacancies', params=params)
     response.raise_for_status()
     review_result = response.json()
     pprint(review_result)
+
 
 
 if __name__ == '__main__':
