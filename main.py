cd0b2e3af987ccb5053f3ec0f50c949b0cdb93d2
Денис Петровский <petrovsky.dv@gmail.com>
2021-01-30 16:53:46 +0300

    добавил получени данных по всм страницам

--- main.py
+++ main.py
@@ -4,33 +4,51 @@ from pprint import pprint
 
 def main():
     programming_languages = ['python', 'java', 'javascript', 'C#', 'C++', 'PHP', 'Typescript', 'Ruby']
-    # programming_languages = ['python']
-    vacancy_statistics = {}
-    for programming_language in programming_languages:
-        vacancy_statistics[programming_language] = fetch_vacancies(programming_language)
-    pprint(vacancy_statistics)
-
-
-def fetch_vacancies(programming_language):
     params = {
         'area': 1,
         'specialization': '1.221',
-        'period': 30,
-        'text': programming_language
+        'period': 30
     }
-    response = requests.get('https://api.hh.ru/vacancies', params=params)
-    response.raise_for_status()
-    review_result = response.json()
+    # programming_languages = ['python']
+    vacancy_statistics = {}
+    for programming_language in programming_languages:
+        print(programming_language)
+        vacancies, vacancies_found = fetch_vacancies(programming_language, params)
+        vacancy_statistics[programming_language] = calculate_average_salary(vacancies, vacancies_found)
+    pprint(vacancy_statistics)
+
+
+def fetch_vacancies(programming_language, params):
+    params = {
+        'text': programming_language,
+    }
+
+    page = 0
+    pages_number = 1
+    vacancies = []
+    while page < pages_number:
+        params['page'] = page
+        print(f'processed page {page}')
+        response = requests.get('https://api.hh.ru/vacancies', params=params)
+        response.raise_for_status()
+        review_result = response.json()
+        vacancies.extend(review_result['items'])
+        pages_number = review_result['pages']
+        vacancies_found = review_result['found']
+        page += 1
+    return vacancies, vacancies_found
+
+
+def calculate_average_salary(vacancies, vacancies_found):
     average_salaries = {}
-    average_salaries['vacancies_found'] = review_result['found']
+    average_salaries['vacancies_found'] = vacancies_found
     salaries = []
-    for vacancy in review_result['items']:
+    for vacancy in vacancies:
         expected_salary = predict_rub_salary(vacancy)
         if expected_salary:
             salaries.append(expected_salary)
     average_salaries['vacancies_processed'] = len(salaries)
     average_salaries['average_salary'] = int(sum(salaries) / len(salaries))
-    pprint(average_salaries)
     return average_salaries
 
 
