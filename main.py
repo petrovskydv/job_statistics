b378bb003cf69e02b27e458a6350a9648bbd4c91
Денис Петровский <petrovsky.dv@gmail.com>
2021-01-31 14:30:43 +0300

    посчитал среднюю зп по ЯП на SJ, вынес токен SJ в переменные окружения

--- main.py
+++ main.py
@@ -1,22 +1,26 @@
 import requests
 from pprint import pprint
 import math
+from dotenv import load_dotenv
+import os
 
 COEFFICIENT_LOWER_SALARY = 1.2
 COEFFICIENT_HIGHER_SALARY = 0.8
 
 
 def main():
+    load_dotenv()
+    superjob_token = os.environ['SUPERJOB_TOKEN']
     programming_languages = ['python', 'java', 'javascript', 'C#', 'C++', 'PHP', 'Typescript', 'Ruby']
     # fetch_headhunter_vacancies(programming_languages)
-    fetch_superjob_vacancies(programming_languages)
+    fetch_superjob_vacancies(programming_languages, superjob_token)
 
 
-def fetch_superjob_vacancies(programming_languages):
+def fetch_superjob_vacancies(programming_languages, superjob_token):
     results_number = 100
     params = {
         'keywords[1][srws]': 1,
-        'keywords[1][skws]': '',
+        'keywords[1][skws]': 'or',
         'keywords[1][keys]': '',
         'page': 0,
         'count': results_number,
@@ -25,25 +29,35 @@ def fetch_superjob_vacancies(programming_languages):
     }
 
     headers = {
-        'X-Api-App-Id': 'v3.r.15222203.452d44010b7ce1097d1d6b3c2a3bf13204c0cf9b.c438b5820baa0e0d31d95ffde59c54bf70c0c618'
+        'X-Api-App-Id': superjob_token
     }
 
+    vacancy_statistics = {}
+    for programming_language in programming_languages:
+        print(programming_language)
+        vacancies, vacancies_found = fetch_vacancies_for_programming_language_superjob(headers, params,
+                                                                                       programming_language)
+        vacancy_statistics[programming_language] = calculate_average_salary_for_superjob(vacancies, vacancies_found)
+    pprint(vacancy_statistics)
+
+
+def fetch_vacancies_for_programming_language_superjob(headers, params, programming_language):
+    params['keywords[1][keys]'] = programming_language
     page = 0
     pages_number = 1
     vacancies = []
+    vacancies_found = 0
     while page < pages_number:
         params['page'] = page
         print(f'processed page {page}')
         response = requests.get('https://api.superjob.ru/2.0/vacancies', params=params, headers=headers)
         response.raise_for_status()
         review_result = response.json()
-        # pprint(review_result)
         vacancies.extend(review_result['objects'])
         vacancies_found = review_result['total']
-        pages_number = math.ceil(vacancies_found / results_number)
+        pages_number = math.ceil(vacancies_found / 100)
         page += 1
-        for vacancy in vacancies:
-            print(vacancy['profession'], vacancy['town']['title'], predict_rub_salary_for_superjob(vacancy))
+    return vacancies, vacancies_found
 
 
 def predict_rub_salary_for_superjob(vacancy):
@@ -94,6 +108,18 @@ def calculate_average_salary(vacancies, vacancies_found):
     salaries = []
     for vacancy in vacancies:
         expected_salary = predict_rub_salary(vacancy)
+        if expected_salary:
+            salaries.append(expected_salary)
+    average_salaries['vacancies_processed'] = len(salaries)
+    average_salaries['average_salary'] = int(sum(salaries) / len(salaries))
+    return average_salaries
+
+
+def calculate_average_salary_for_superjob(vacancies, vacancies_found):
+    average_salaries = {'vacancies_found': vacancies_found}
+    salaries = []
+    for vacancy in vacancies:
+        expected_salary = predict_rub_salary_for_superjob(vacancy)
         if expected_salary:
             salaries.append(expected_salary)
     average_salaries['vacancies_processed'] = len(salaries)
