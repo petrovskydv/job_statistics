c6b7abe42e2c5f4bfe667aafac986e63a90ddf02
Денис Петровский <petrovsky.dv@gmail.com>
2021-01-31 16:55:23 +0300

    почистил код

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import logging
 import math
 import os
 
@@ -5,47 +6,67 @@ import requests
 from dotenv import load_dotenv
 from terminaltables import AsciiTable
 
+logger = logging.getLogger(__name__)
+
 COEFFICIENT_LOWER_SALARY = 1.2
 COEFFICIENT_HIGHER_SALARY = 0.8
+RESULTS_ON_PAGE_NUMBER = 100
 
 
 def main():
+    logging.basicConfig(format="%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+    logger.setLevel(logging.INFO)
+
     load_dotenv()
     superjob_token = os.environ['SUPERJOB_TOKEN']
+
     programming_languages = ['python', 'java', 'javascript', 'C#', 'C++', 'PHP', 'Typescript', 'Ruby']
-    headhunter_vacancies = fetch_headhunter_vacancies(programming_languages)
-    superjob_vacancies = fetch_superjob_vacancies(programming_languages, superjob_token)
-    show_statistics(headhunter_vacancies, 'HeadHunter Moscow')
-    show_statistics(superjob_vacancies, 'SuperJob Moscow')
+
+    headhunter_region = 1
+    headhunter_specialization = '1.221'
+    vacancy_publication_period = 30
+    superjob_region = 4
+    superjob_specialization = 48
+
+    logger.info('getting statistics for HeadHunter')
+    headhunter_languages_statistics = fetch_headhunter_vacancy_statistics(
+        programming_languages, headhunter_region, headhunter_specialization, vacancy_publication_period)
+    logger.info('getting statistics for SuperJob')
+    superjob_languages_statistics = fetch_superjob_vacancy_statistics(
+        programming_languages, superjob_token, superjob_specialization, superjob_region)
+    show_statistics(headhunter_languages_statistics, 'HeadHunter Moscow')
+    show_statistics(superjob_languages_statistics, 'SuperJob Moscow')
 
 
-def fetch_headhunter_vacancies(programming_languages):
+def fetch_headhunter_vacancy_statistics(
+        programming_languages, headhunter_region, headhunter_specialization, vacancy_publication_period):
+
     params = {
-        'area': 1,
-        'specialization': '1.221',
-        'period': 30,
+        'area': headhunter_region,
+        'specialization': headhunter_specialization,
+        'period': vacancy_publication_period,
         'page': 0,
-        'per_page': 100,
+        'per_page': RESULTS_ON_PAGE_NUMBER,
         'text': ''
     }
     vacancy_statistics = {}
     for programming_language in programming_languages:
-        print(programming_language)
-        vacancies, vacancies_found = fetch_vacancies(programming_language, params)
-        vacancy_statistics[programming_language] = calculate_average_salary(vacancies, vacancies_found)
+        logger.debug(programming_language)
+        vacancies, vacancies_found = fetch_vacancies_for_headhunter(programming_language, params)
+        vacancy_statistics[programming_language] = fetch_statistics_for_programming_language(
+            vacancies, vacancies_found, predict_rub_salary_for_headhunter)
     return vacancy_statistics
 
 
-def fetch_vacancies(programming_language, params):
+def fetch_vacancies_for_headhunter(programming_language, params):
     params['text'] = programming_language
-
     page = 0
     pages_number = 1
     vacancies_found = 0
     vacancies = []
     while page < pages_number:
         params['page'] = page
-        print(f'processed page {page}')
+        logger.debug(f'processed page {page}')
         response = requests.get('https://api.hh.ru/vacancies', params=params)
         response.raise_for_status()
         review_result = response.json()
@@ -56,11 +77,11 @@ def fetch_vacancies(programming_language, params):
     return vacancies, vacancies_found
 
 
-def calculate_average_salary(vacancies, vacancies_found):
+def fetch_statistics_for_programming_language(vacancies, vacancies_found, callback):
     average_salaries = {'vacancies_found': vacancies_found}
     salaries = []
     for vacancy in vacancies:
-        expected_salary = predict_rub_salary(vacancy)
+        expected_salary = callback(vacancy)
         if expected_salary:
             salaries.append(expected_salary)
     average_salaries['vacancies_processed'] = len(salaries)
@@ -84,16 +105,16 @@ def show_statistics(vacancies, title):
     print(table.table)
 
 
-def fetch_superjob_vacancies(programming_languages, superjob_token):
-    results_number = 100
+def fetch_superjob_vacancy_statistics(programming_languages, superjob_token,
+                                      superjob_specialization, superjob_region):
     params = {
         'keywords[1][srws]': 1,
         'keywords[1][skws]': 'or',
         'keywords[1][keys]': '',
         'page': 0,
-        'count': results_number,
-        'catalogues': 48,
-        'town': 4
+        'count': RESULTS_ON_PAGE_NUMBER,
+        'catalogues': superjob_specialization,
+        'town': superjob_region
     }
 
     headers = {
@@ -102,10 +123,11 @@ def fetch_superjob_vacancies(programming_languages, superjob_token):
 
     vacancy_statistics = {}
     for programming_language in programming_languages:
-        print(programming_language)
-        vacancies, vacancies_found = fetch_vacancies_for_programming_language_superjob(headers, params,
-                                                                                       programming_language)
-        vacancy_statistics[programming_language] = calculate_average_salary_for_superjob(vacancies, vacancies_found)
+        logger.debug(programming_language)
+        vacancies, vacancies_found = fetch_vacancies_for_programming_language_superjob(
+            headers, params, programming_language)
+        vacancy_statistics[programming_language] = fetch_statistics_for_programming_language(
+            vacancies, vacancies_found, predict_rub_salary_for_superjob)
     return vacancy_statistics
 
 
@@ -117,27 +139,15 @@ def fetch_vacancies_for_programming_language_superjob(headers, params, programmi
     vacancies_found = 0
     while page < pages_number:
         params['page'] = page
-        print(f'processed page {page}')
+        logger.debug(f'processed page {page}')
         response = requests.get('https://api.superjob.ru/2.0/vacancies', params=params, headers=headers)
         response.raise_for_status()
         review_result = response.json()
         vacancies.extend(review_result['objects'])
         vacancies_found = review_result['total']
-        pages_number = math.ceil(vacancies_found / 100)
+        pages_number = math.ceil(vacancies_found / RESULTS_ON_PAGE_NUMBER)
         page += 1
     return vacancies, vacancies_found
-
-
-def calculate_average_salary_for_superjob(vacancies, vacancies_found):
-    average_salaries = {'vacancies_found': vacancies_found}
-    salaries = []
-    for vacancy in vacancies:
-        expected_salary = predict_rub_salary_for_superjob(vacancy)
-        if expected_salary:
-            salaries.append(expected_salary)
-    average_salaries['vacancies_processed'] = len(salaries)
-    average_salaries['average_salary'] = int(sum(salaries) / len(salaries))
-    return average_salaries
 
 
 def predict_rub_salary_for_superjob(vacancy):
@@ -146,7 +156,7 @@ def predict_rub_salary_for_superjob(vacancy):
     return predict_salary(vacancy['payment_from'], vacancy['payment_to'])
 
 
-def predict_rub_salary(vacancy):
+def predict_rub_salary_for_headhunter(vacancy):
     if not vacancy['salary']:
         return None
     if vacancy['salary']['currency'] != 'RUR':
